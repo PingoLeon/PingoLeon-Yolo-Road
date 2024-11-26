b8ac04691e3f2bb44ff27060ec1e4f4a527103a8
PingoLéon <leon.dalle@edu.ece.fr>
2024-11-27 02:04:42 +0900

    🔧 Retravail de la structure du projet

--- YoloV11/trainYOLO11.py
+++ YoloV11/trainYOLO11.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
 
 # Charger le modèle de détection YOLOv8
-model = YOLO('yolo11n.pt')
+model = YOLO('YoloV11/yolo11n.pt')
 
 # Lancer l'entraînement
 if __name__ == '__main__':
     results = model.train(
-        data='data.yaml',
+        data='YoloV11/data11.yaml',
         epochs=50,
         imgsz=640,
         batch=8,
-        name='yolov11_Detection',
+        name='testv11',
         cache=True,
     )
